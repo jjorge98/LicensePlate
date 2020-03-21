5b37858b503fd043275fc4198a08901af95e4f2b
Jorge Júnior <jjorge98@outlook.com>
2020-03-21 03:14:48 -0300

    'First'

---
+++ app/src/main/java/br/com/licenseplate/BeginClient.kt
@@ -0,0 +1,12 @@
+package br.com.licenseplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BeginClient : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_begin_client)
+    }
+}
