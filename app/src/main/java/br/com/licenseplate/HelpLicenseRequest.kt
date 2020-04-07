d5fdaab44efcb046df8a09b11a54c738603cc47a
jjorge98 <jjorge98@outlook.com>
2020-04-07 03:15:38 -0300

    'RequestList_AutorizationClass'

--- app/src/main/java/br/com/licenseplate/HelpLicenseRequest.kt
+++ app/src/main/java/br/com/licenseplate/HelpLicenseRequest.kt
@@ -1,7 +1,9 @@
 package br.com.licenseplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class HelpLicenseRequest : AppCompatActivity() {
 
