8564de92c5306d2dd2c21dffd83eaf2e8c863436
Jorge Junior <jjorge98@outlook.com>
2020-04-09 18:52:30 -0300

    'SolicitarPlacav1'

--- app/src/main/java/br/com/licenseplate/RequestLicense.kt
+++ app/src/main/java/br/com/licenseplate/RequestLicense.kt
@@ -1,10 +1,7 @@
 package br.com.licenseplate
 
-import android.app.Activity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -13,13 +10,11 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_request_license.*
 
 class RequestLicense : AppCompatActivity() {
+    private lateinit var estado: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_request_license)
-
-        helpTextReqLicense.visibility = View.GONE
-        autNumberReqLicense.visibility = View.GONE
-        buttonReqLicense.visibility = View.GONE
 
         //Acha o spinner pelo id
         val spinner: Spinner = findViewById(R.id.spinnerEstados)
@@ -36,22 +31,86 @@ class RequestLicense : AppCompatActivity() {
             spinner.adapter = adapter
         }
 
+        //Adiciona um listener no spinner
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            //Se nada for selecionado, não faz nada
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            //Se algo for selecionado, alguns campos vão aparecer. Essa função recebe 4 parametros.
+            //Os que mais importam pra gente são parent que é o adapterview com os itens do spinner
+            // e a posição que é a que o usuário selecionou
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                //Variável que pega o item que o usuário selecionou
+                estado = parent?.getItemAtPosition(position).toString()
+
+                if (estado == "BA") {
+                    //Coloca no textview o que fazer e nos inputs a hint do que colocar
+                    helpTextReqLicense.text = resources.getString(R.string.textLicense)
+                    inputReqLicense.hint = resources.getString(R.string.license)
+                    input2ReqLicense.visibility = View.GONE
+                } else {
+                    helpTextReqLicense.text = resources.getString(R.string.textAutorizationChassi)
+                    inputReqLicense.hint = resources.getString(R.string.autorization)
+                    input2ReqLicense.hint = resources.getString(R.string.chassi)
+                    input2ReqLicense.visibility = View.VISIBLE
+                }
+            }
+        }
+
         buttonReqLicense.setOnClickListener { next() }
     }
 
     private fun next() {
-        val autorizacao = autNumberReqLicense.text.toString()
+        if (estado == "BA") {
+            val placa = inputReqLicense.text.toString()
 
-        if (autorizacao.isEmpty()) {
-            Toast.makeText(this, "Por favor, digite um número!", Toast.LENGTH_LONG).show()
-            return
-        } else if (autorizacao.length < 15) {
-            Toast.makeText(
-                this,
-                "Por favor, verifique o número da autorização e tente novamente.",
-                Toast.LENGTH_LONG
-            ).show()
-            return
+            if (placa.isEmpty()) {
+                Toast.makeText(this, "Por favor, digite uma placa", Toast.LENGTH_LONG).show()
+                return
+            } else if (placa.length != 7) {
+                Toast.makeText(
+                    this,
+                    "Por favor, digite uma placa válida! Formato válido: AAA0A00",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                //Enviar pra próxima activity ou api
+            }
+        } else {
+            val autorizacao = inputReqLicense.text.toString()
+            val chassi = input2ReqLicense.text.toString().toUpperCase()
+
+            //Ver se tem como simplificar esse if
+            if ('I' in chassi || 'O' in chassi || 'Q' in chassi) {
+                Toast.makeText(
+                    this,
+                    "Chassi não obedece as normas técnicas da ABNT. Por favor, verifique o chassi e tente novamente",
+                    Toast.LENGTH_LONG
+                ).show()
+                return
+            }
+
+            if (autorizacao.isEmpty() || chassi.isEmpty()) {
+                Toast.makeText(this, "Por favor, preencha todos os campos!", Toast.LENGTH_LONG)
+                    .show()
+                return
+            } else if (autorizacao.length < 15) {
+                Toast.makeText(
+                    this,
+                    "Por favor, verifique a autorização informada e tente novamente!",
+                    Toast.LENGTH_LONG
+                ).show()
+                return
+            } else {
+                //Enviar pra próxima activity ou api
+            }
         }
     }
 }
